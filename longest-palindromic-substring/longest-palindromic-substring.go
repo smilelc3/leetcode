63c2f6e0632a893af2af30939b228c647c628f21
smilelc3 <smile@liuchang.men>
2021-01-05 23:46:59 +0800

    manacher算法

--- longest-palindromic-substring/longest-palindromic-substring.go
+++ longest-palindromic-substring/longest-palindromic-substring.go
@@ -10,47 +10,47 @@ func longestPalindrome(s string) string {
 	for _, char := range s {
 		manacherString += string(char) + "#"
 	}
-	radiusArray := make([]int, len(manacherString)) // 记录每个位置的回文半径
+	P := make([]int, len(manacherString)) // 记录每个位置的回文半径
 
 	// manacher算法核心
-	R := -1      // R是最右回文边界
-	C := -1      // C是R对应的最左回文中心
-	maxR := 0    // maxR记录最大的回文半径
-	maxRIdx := 0 // 记录最大回文半径的下标
-	for idx := 0; idx < len(manacherString); idx++ {
+	R := -1    // R是最右回文边界
+	C := -1    // C是R对应的最左回文中心
+	maxR := 0  // maxR记录最大的回文半径
+	maxRi := 0 // 记录最大回文半径的下标
+	for i := 0; i < len(manacherString); i++ {
 		//第一步直接取得可能的最短的回文半径，
-		//当idx>R时，最短的回文半径是1，
+		//当i>=R时，最短的回文半径是1，
 		//反之，最短的回文半径可能是i对应的i'的回文半径或者i到R的距离
-		if R > idx {
-			radiusArray[idx] = minInt(R-idx, radiusArray[2*C-idx])
+		if i >= R {
+			P[i] = 1
 		} else {
-			radiusArray[idx] = 1
+			P[i] = min(R-i, P[2*C-i])
+
 		}
-		for idx+radiusArray[idx] < len(manacherString) && idx-radiusArray[idx] > -1 {
-			if manacherString[idx+radiusArray[idx]] ==
-				manacherString[idx-radiusArray[idx]] {
-				radiusArray[idx]++
+		for i+P[i] < len(manacherString) && i-P[i] >= 0 {
+			if manacherString[i+P[i]] == manacherString[i-P[i]] {
+				P[i]++ //更新当前位置的回文半径
 			} else {
 				break
 			}
 		}
-		if idx+radiusArray[idx] > R {
-			R = idx + radiusArray[idx]
-			C = idx
+		if i+P[i] > R {
+			R = i + P[i]
+			C = i
 		}
 
-		if maxR < radiusArray[idx] {
-			maxR = radiusArray[idx]
-			maxRIdx = idx
+		if maxR < P[i] {
+			maxR = P[i]
+			maxRi = i
 		}
 	}
-	//println(maxR - 1)
-	//manacherStringLongestPalindrome := manacherString[maxRIdx-(maxR-1) : maxRIdx+(maxR-1)+1]
+	// 最长回文半径为 maxR - 1
+	//manacherStringLongestPalindrome := manacherString[maxRi-(maxR-1) : maxRi+(maxR-1)+1]
 	//return strings.Replace(manacherStringLongestPalindrome, "#", "", -1)
-	return s[(maxRIdx-(maxR-1))/2 : (maxRIdx+(maxR-1)+1)/2]
+	return s[(maxRi-(maxR-1))/2 : (maxRi+(maxR-1)+1)/2]
 }
 
-func minInt(x, y int) int {
+func min(x, y int) int {
 	if x < y {
 		return x
 	}
