325f9977b3de60caf501809c198d0fe4716765c7
smilelc3 <smile@liuchang.men>
2021-01-07 22:52:31 +0800

    13. `Roman to Integer`

--- roman-to-integer/roman-to-integer.go
+++ roman-to-integer/roman-to-integer.go
@@ -1,22 +1,21 @@
 package roman_to_integer
 
 /*
-	Symbol       Value
-	I             1
-	IV			  4
-	V             5
-	IX			  9
-	X             10
-	XL			  40
-	L             50
-	XC			  90
-	C             100
-	CD			  400
-	D             500
-	CM			  900
-	M             1000
+	Symbol      Value
+	I           1
+	IV          4
+	V           5
+	IX          9
+	X           10
+	XL          40
+	L           50
+	XC          90
+	C           100
+	CD          400
+	D           500
+	CM          900
+	M           1000
 */
-
 func romanToInt(s string) int {
 	idx := 0
 	romanNum := 0
