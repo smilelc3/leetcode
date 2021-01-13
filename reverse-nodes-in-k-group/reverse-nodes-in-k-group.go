b08e3cfcff58259f26a2c89845e1a8f818056ba4
smilelc3 <smile@liuchang.men>
2021-01-13 22:47:05 +0800

    25. `Reverse Nodes in k-Group`

---
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
@@ -0,0 +1,63 @@
+package reverse_nodes_in_k_group
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	// space cost O(1)
+	var reverseHead *ListNode
+	var preNode, startNode, endNode, nextNode *ListNode
+	idx := 0
+	pNode := head
+	for pNode != nil {
+		if idx%k == 0 {
+			startNode = pNode
+		}
+		if idx%k == k-1 {
+			endNode = pNode
+			nextNode = pNode.Next
+			// find a k group
+			reverseSinglyLink(startNode, endNode)
+			startNode, endNode = endNode, startNode
+			if reverseHead == nil {
+				reverseHead = startNode
+			}
+			if preNode != nil {
+				preNode.Next = startNode
+			}
+			if endNode != nil {
+				endNode.Next = nextNode
+			}
+			preNode = endNode
+			pNode = nextNode
+		} else {
+			pNode = pNode.Next
+		}
+		idx++
+	}
+	return reverseHead
+}
+
+// 反转单向链表
+func reverseSinglyLink(startNode, endNode *ListNode) {
+	// reverse a link return head Node
+	pNode := startNode
+	var nextNode, preNode *ListNode
+	for preNode != endNode {
+		nextNode = pNode.Next
+		// reverse node
+		pNode.Next = preNode
+		preNode = pNode
+		pNode = nextNode
+	}
+}
---
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group_test.go
@@ -0,0 +1,86 @@
+package reverse_nodes_in_k_group
+
+import "testing"
+
+func TestReverseKGroupExample1(t *testing.T) {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+	k := 2
+	ans := reverseKGroup(head, k)
+	if ans.Val != 2 ||
+		ans.Next.Val != 1 ||
+		ans.Next.Next.Val != 4 ||
+		ans.Next.Next.Next.Val != 3 ||
+		ans.Next.Next.Next.Next.Val != 5 ||
+		ans.Next.Next.Next.Next.Next != nil {
+		t.Error("wrong Links")
+	}
+}
+
+func TestReverseKGroupExample2(t *testing.T) {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+	k := 3
+	ans := reverseKGroup(head, k)
+	if ans.Val != 3 ||
+		ans.Next.Val != 2 ||
+		ans.Next.Next.Val != 1 ||
+		ans.Next.Next.Next.Val != 4 ||
+		ans.Next.Next.Next.Next.Val != 5 ||
+		ans.Next.Next.Next.Next.Next != nil {
+		t.Error("wrong Links")
+	}
+}
+
+func TestReverseKGroupExample3(t *testing.T) {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+	k := 1
+	ans := reverseKGroup(head, k)
+	if ans.Val != 1 ||
+		ans.Next.Val != 2 ||
+		ans.Next.Next.Val != 3 ||
+		ans.Next.Next.Next.Val != 4 ||
+		ans.Next.Next.Next.Next.Val != 5 ||
+		ans.Next.Next.Next.Next.Next != nil {
+		t.Error("wrong Links")
+	}
+}
+
+func TestReverseKGroupExample4(t *testing.T) {
+	head := &ListNode{Val: 1}
+	k := 1
+	ans := reverseKGroup(head, k)
+	if ans.Val != 1 ||
+		ans.Next != nil {
+		t.Error("wrong Links")
+	}
+}
+
+func TestReverseSingleLink(t *testing.T) {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+	tail := head.Next.Next.Next.Next
+	reverseSinglyLink(head, tail)
+	head, tail = tail, head
+	if head.Val != 5 ||
+		head.Next.Val != 4 ||
+		head.Next.Next.Val != 3 ||
+		head.Next.Next.Next.Val != 2 ||
+		head.Next.Next.Next.Next.Val != 1 ||
+		head.Next.Next.Next.Next.Next != nil {
+		t.Error("wrong Links")
+	}
+}
