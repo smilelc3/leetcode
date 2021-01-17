97ef29b523e03420060769ad1bc7c969316ce410
smilelc3 <smile@liuchang.men>
2021-01-17 11:04:23 +0800

    replace all Tab by 4 spaces

--- integer-to-roman/integer-to-roman.go
+++ integer-to-roman/integer-to-roman.go
@@ -3,20 +3,20 @@ package integer_to_roman
 import "strings"
 
 /*
-	Symbol      Value
-	I           1
-	IV          4
-	V           5
-	IX          9
-	X           10
-	XL          40
-	L           50
-	XC          90
-	C           100
-	CD          400
-	D           500
-	CM          900
-	M           1000
+   Symbol      Value
+   I           1
+   IV          4
+   V           5
+   IX          9
+   X           10
+   XL          40
+   L           50
+   XC          90
+   C           100
+   CD          400
+   D           500
+   CM          900
+   M           1000
 */
 
 func intToRoman(num int) string {
