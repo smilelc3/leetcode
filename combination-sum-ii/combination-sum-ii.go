b53c4598524933d0d967d23e382ec90dbc80a9e1
smilelc3 <smile@liuchang.men>
2021-01-18 01:50:44 +0800

    40. Combination Sum II

--- combination-sum-ii/combination-sum-ii.go
+++ combination-sum-ii/combination-sum-ii.go
@@ -7,14 +7,61 @@ import (
 )
 
 func combinationSum2(candidates []int, target int) [][]int {
+	// version 1: using Map to remove duplicates
+	//return combinationSum2WithMapDeduplication(candidates, target)
+
+	// version 2: edit "for" condition to remove duplicates
+	return combinationSum2Best(candidates, target)
+}
+
+func combinationSum2Best(candidates []int, target int) [][]int {
 	var combinations [][]int
-	combinationsStringSet := map[string]bool{}
 	sort.Ints(candidates)
-	deepFirstSearchCombination2(candidates, target, []int{}, 0, &combinations, &combinationsStringSet)
+	deepFirstSearchCombination2Best(candidates, target, []int{}, 0, &combinations)
 	return combinations
 }
 
-func deepFirstSearchCombination2(candidates []int, target int, searchResult []int, searchIdx int, pCombinations *[][]int, pCombinationsStringSet *map[string]bool) {
+func deepFirstSearchCombination2Best(candidates []int, target int, searchResult []int, searchIdx int, pCombinations *[][]int) {
+	for idx := searchIdx; idx < len(candidates); idx++ {
+		// magic pruning！
+		if idx > searchIdx && candidates[idx] == candidates[idx-1] {
+			continue
+		}
+		num := candidates[idx]
+		if target > num {
+			searchResult = append(searchResult, num)
+			nextTarget := target - num
+			nextIdx := idx + 1
+			if candidates[idx] <= nextTarget {
+				deepFirstSearchCombination2Best(candidates, nextTarget, searchResult, nextIdx, pCombinations)
+			}
+			searchResult = searchResult[:len(searchResult)-1]
+
+		} else if target == num {
+			searchResult = append(searchResult, num)
+			// deep copy
+			searchResultBack := make([]int, len(searchResult))
+			copy(searchResultBack, searchResult)
+			*pCombinations = append(*pCombinations, searchResultBack)
+
+			break // Pruning
+
+		} else {
+			// num > target
+			break // Pruning
+		}
+	}
+}
+
+func combinationSum2WithMapDeduplication(candidates []int, target int) [][]int {
+	var combinations [][]int
+	combinationsStringSet := map[string]bool{}
+	sort.Ints(candidates)
+	deepFirstSearchCombination2MapDeduplication(candidates, target, []int{}, 0, &combinations, &combinationsStringSet)
+	return combinations
+}
+
+func deepFirstSearchCombination2MapDeduplication(candidates []int, target int, searchResult []int, searchIdx int, pCombinations *[][]int, pCombinationsStringSet *map[string]bool) {
 	for idx := searchIdx; idx < len(candidates); idx++ {
 		num := candidates[idx]
 		if target > num {
@@ -22,7 +69,7 @@ func deepFirstSearchCombination2(candidates []int, target int, searchResult []in
 			nextTarget := target - num
 			nextIdx := idx + 1
 			if candidates[idx] <= nextTarget {
-				deepFirstSearchCombination2(candidates, nextTarget, searchResult, nextIdx, pCombinations, pCombinationsStringSet)
+				deepFirstSearchCombination2MapDeduplication(candidates, nextTarget, searchResult, nextIdx, pCombinations, pCombinationsStringSet)
 			}
 			searchResult = searchResult[:len(searchResult)-1]
 
