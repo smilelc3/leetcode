59133e5d0d5526997788fdced40376b0c2718085
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-04-09 09:52:31 +0800

    121. Best Time to Buy and Sell Stock

---
+++ leetcode-cpp/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+//
+// Created by smile on 2021/4/9.
+//
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+class Solution {
+public:
+    static int maxProfit(std::vector<int> &prices) {
+        int maxProfitVal = 0;
+        int preMinPrice = std::numeric_limits<int>::max();
+        for (int & price : prices) {
+            maxProfitVal = std::max(maxProfitVal, price - preMinPrice);
+            preMinPrice = std::min(preMinPrice, price);
+        }
+        return maxProfitVal;
+    }
+};
