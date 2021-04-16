5fe2e5908abc57941a2bf10fdfabeafa342dfc3a
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-04-16 17:36:33 +0800

    122. Best Time to Buy and Sell Stock II

--- leetcode-cpp/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii-test.cpp
+++ leetcode-cpp/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii-test.cpp
@@ -10,15 +10,15 @@
 namespace {
     TEST(maxProfitTest, Example1) {
         auto prices = std::vector<int>{7, 1, 5, 3, 6, 4};
-        int ans = Solution::maxProfit(prices);
+        int ans = (new Solution)->maxProfit(prices);
         int rightAns = 5;
         ASSERT_EQ(ans, rightAns);
     }
 
     TEST(maxProfitTest, Example2) {
-        auto prices = std::vector<int>{7, 6, 4, 3, 1};
-        int ans = Solution::maxProfit(prices);
-        int rightAns = 0;
+        auto prices = std::vector<int>{7, 1, 5, 3, 6, 4};
+        int ans = (new Solution)->maxProfit(prices);
+        int rightAns = 5;
         ASSERT_EQ(ans, rightAns);
     }
 }
@@ -27,3 +27,4 @@ namespace {
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
