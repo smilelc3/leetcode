362fd126ce4aa4b150cb1437ee68d9e3d27189ec
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-04-29 11:05:22 +0800

    133. Clone Graph

--- leetcode-cpp/clone-graph/clone-graph.cpp
+++ leetcode-cpp/clone-graph/clone-graph.cpp
@@ -28,48 +28,45 @@ public:
 };
 
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
 
 class Solution {
 public:
     // 无向图的复制
-    Node *cloneGraph(Node *node) {
+    Node *cloneGraph(Node* const node) {
         if (node == nullptr) {
             return nullptr;
         }
-        std::queue<decltype(node)> nodeQueue;
+        std::queue<Node*> nodeQueue;
         nodeQueue.emplace(node);
+        auto nodeCopy = new Node(node->val);
+        rawNode2CpyNodeMap[node] = nodeCopy;
 
-        decltype(node)  nodeCopy;
         while (not nodeQueue.empty()) {
             auto pNode = nodeQueue.front();
-
             nodeQueue.pop();
-            decltype(pNode) pNodeCopy;
-            if (rawNode2CpyNodeMap.find(pNode) == rawNode2CpyNodeMap.end()) {
-                pNodeCopy = new Node(pNode->val);
-            } else {
-                pNodeCopy = rawNode2CpyNodeMap.at(pNode);
-            }
-            if (pNode == node) {
-                nodeCopy = pNodeCopy;
-            }
+            auto pNodeCopy = rawNode2CpyNodeMap.at(pNode);
             for (const auto &neighbor : pNode->neighbors) {
                 if (rawNode2CpyNodeMap.find(neighbor) == rawNode2CpyNodeMap.end()) {
                     // neighbor 节点之前未创建
                     auto neighborCopy = new Node(neighbor->val);
                     pNodeCopy->neighbors.emplace_back(neighborCopy);
                     rawNode2CpyNodeMap[neighbor] = neighborCopy;
-                    nodeQueue.emplace(neighbor);
+                    if (visitNodesSet.find(neighbor) == visitNodesSet.end()) {
+                        nodeQueue.emplace(neighbor);
+                    }
                 } else {
                     // neighbor 节点之前已经创建
                     pNodeCopy->neighbors.emplace_back(rawNode2CpyNodeMap.at(neighbor));
                 }
             }
+            visitNodesSet.insert(pNode);
         }
         return nodeCopy;
     }
 
 private:
     std::unordered_map<Node *, Node *> rawNode2CpyNodeMap;
+    std::unordered_set<Node *> visitNodesSet;
 };
