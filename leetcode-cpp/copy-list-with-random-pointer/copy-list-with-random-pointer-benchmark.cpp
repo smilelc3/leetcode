b1aafbb899b7d4de867f4bad5155b6780dadfa9b
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-05-09 03:13:18 +0800

    138 添加benchmark

---
+++ leetcode-cpp/copy-list-with-random-pointer/copy-list-with-random-pointer-benchmark.cpp
@@ -0,0 +1,57 @@
+//
+// Created by smile on 2021/5/9.
+//
+
+#include <benchmark/benchmark.h>
+#include "copy-list-with-random-pointer.cpp"
+
+// copyRandomListUsingTwoTraverWithMap  time: O(2n) space: O(n)
+static void benchCopyRandomListUsingTwoTraverWithMap(benchmark::State &state) {
+    auto head = Node(7);
+    auto Node1 = Node(13);
+    auto Node2 = Node(11);
+    auto Node3 = Node(10);
+    auto Node4 = Node(1);
+
+    head.next = &Node1;
+    Node1.next = &Node2;
+    Node2.next = &Node3;
+    Node3.next = &Node4;
+
+    Node1.random = &head;
+    Node3.random = &Node2;
+    Node4.random = &head;
+
+    for (auto _: state) {
+        Solution().copyRandomListUsingTwoTraverWithMap(&head);
+    }
+}
+
+BENCHMARK(benchCopyRandomListUsingTwoTraverWithMap);    // NOLINT
+
+
+// copyRandomListUsingThreeTraver  time: O(3n) space: O(1)
+static void benchCopyRandomListUsingThreeTraver(benchmark::State &state) {
+    auto head = Node(7);
+    auto Node1 = Node(13);
+    auto Node2 = Node(11);
+    auto Node3 = Node(10);
+    auto Node4 = Node(1);
+
+    head.next = &Node1;
+    Node1.next = &Node2;
+    Node2.next = &Node3;
+    Node3.next = &Node4;
+
+    Node1.random = &head;
+    Node3.random = &Node2;
+    Node4.random = &head;
+    for (auto _: state) {
+        Solution().copyRandomListUsingThreeTraver(&head);
+    }
+}
+
+BENCHMARK(benchCopyRandomListUsingThreeTraver);  // NOLINT
+
+
+BENCHMARK_MAIN();
