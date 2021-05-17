23f4b9f568c37e2680963efbfad56dafbe85a367
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-05-17 10:01:25 +0800

    131. Palindrome Partitioning

--- leetcode-cpp/palindrome-partitioning/palindrome-partitioning.cpp
+++ leetcode-cpp/palindrome-partitioning/palindrome-partitioning.cpp
@@ -8,24 +8,39 @@
 class Solution {
 public:
     std::vector<std::vector<std::string>> partition(const std::string &s) {
-
-        std::vector<std::vector<std::string>> res;
         if (s.empty()) {
             return res;
         }
+        // dp[i][j]表示s[i,j]子字符串是否是回文串
+        dp = std::vector<std::vector<bool>>(s.size(), std::vector<bool>(s.size(), false));
 
-        std::vector<bool> dpOdd(s.size(), true);     // 以i为中心，奇数长度的字符串是否是回文串，初始化半径为0.5
-        std::vector<bool> dpEven(s.size() - 1);      // 以i+0.5中心，偶数长度的字符串是否是回文串，初始化半径为1
-
-        res.emplace_back();
-        for (auto idx = 0; idx < s.size(); idx++) {
-            res[0].emplace_back(s.substr(idx,1));
+        for (int right = 0; right < s.size(); right++) {
+            for (auto left = 0; left <= right; left++) {
+                dp[left][right] = s[left] == s[right] and (right - left <= 1 or dp[left + 1][right - 1]);
+            }
         }
-
-        for (auto idx = 0; idx < dpEven.size(); idx++) {
-            dpEven[idx] = s[idx] == s[idx + 1];
-        }
+        std::vector<std::string> temp;
+        dfsSplit(s, 0, temp);
 
         return res;
     }
+
+private:
+    void dfsSplit(const std::string &s, size_t start, std::vector<std::string> &temp) {
+        if (start == s.size()) {
+            // 保存结果
+            res.emplace_back(temp);
+            return;
+        }
+        for (auto stop = start; stop < s.size(); stop++) {
+            if (dp[start][stop]) {
+                temp.emplace_back(s.substr(start, stop - start + 1));
+                dfsSplit(s, stop + 1, temp);
+                temp.pop_back();
+            }
+        }
+    }
+
+    std::vector<std::vector<std::string>> res;
+    std::vector<std::vector<bool>> dp;
 };
