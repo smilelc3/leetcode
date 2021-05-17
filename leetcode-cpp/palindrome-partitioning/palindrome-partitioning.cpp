541e284e52d0a7556f711b6e43cb437ced3a116b
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-05-17 11:01:12 +0800

    131.空间改进

--- leetcode-cpp/palindrome-partitioning/palindrome-partitioning.cpp
+++ leetcode-cpp/palindrome-partitioning/palindrome-partitioning.cpp
@@ -7,40 +7,43 @@
 
 class Solution {
 public:
-    std::vector<std::vector<std::string>> partition(const std::string &s) {
-        if (s.empty()) {
-            return res;
-        }
-        // dp[i][j]表示s[i,j]子字符串是否是回文串
-        dp = std::vector<std::vector<bool>>(s.size(), std::vector<bool>(s.size(), false));
+    std::vector<std::vector<std::string>> partition(std::string &s) {
+        std::vector<std::vector<std::string>> res;
+        std::vector<std::vector<int>> dp;
 
-        for (int right = 0; right < s.size(); right++) {
-            for (auto left = 0; left <= right; left++) {
-                dp[left][right] = s[left] == s[right] and (right - left <= 1 or dp[left + 1][right - 1]);
+        // dp[i]表示以i起点，所有可以形成回文子串的终点
+        dp.resize(s.size());
+
+        for (auto idx = 0; idx < s.size(); idx++) {
+            for (auto width = 0; width < 2; width++) {
+                auto left = idx, right = idx + width;
+                while (left >= 0 and right < s.size() and s[left] == s[right]) {
+                    dp[left].emplace_back(right);
+                    left--;
+                    right++;
+                }
             }
         }
         std::vector<std::string> temp;
-        dfsSplit(s, 0, temp);
-
+        temp.reserve(s.size());
+        dfsSplit(s, 0, temp, res, dp);
         return res;
     }
 
 private:
-    void dfsSplit(const std::string &s, size_t start, std::vector<std::string> &temp) {
+    void dfsSplit(const std::string &s, size_t start, std::vector<std::string> &temp,
+                  std::vector<std::vector<std::string>> &res, std::vector<std::vector<int>> &dp) {
         if (start == s.size()) {
             // 保存结果
             res.emplace_back(temp);
             return;
         }
-        for (auto stop = start; stop < s.size(); stop++) {
-            if (dp[start][stop]) {
-                temp.emplace_back(s.substr(start, stop - start + 1));
-                dfsSplit(s, stop + 1, temp);
-                temp.pop_back();
-            }
+        for (auto stop : dp[start]) {
+            temp.emplace_back(s.substr(start, stop - start + 1));
+            dfsSplit(s, stop + 1, temp, res, dp);
+            temp.pop_back();
         }
     }
 
-    std::vector<std::vector<std::string>> res;
-    std::vector<std::vector<bool>> dp;
+
 };
