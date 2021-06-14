2cfcc46a15f8b4f84682601d3cb3eaa5bbcc0f7c
smilelc3 <smile@liuchang.men>
2021-06-14 20:40:58 +0800

    fixup! 169. Majority Element 添加benchmark

---
+++ leetcode-cpp/majority-element/majority-element-benchmark.cpp
@@ -0,0 +1,28 @@
+//
+// Created by smile on 2021/6/14.
+//
+
+#include <benchmark/benchmark.h>
+#include "majority-element.cpp"
+
+static std::vector<int> nums = {4, 2, 0, 4, 2, 1, 5, 4, 4, 1, 1, 2, 4, 4, 2, 0, 4, 4};
+
+
+static void bitsMethod(benchmark::State &state) {
+    for (auto _: state) {
+        Solution().bitsMethod(nums);
+    }
+}
+
+BENCHMARK(bitsMethod);      // NOLINT
+
+
+static void majorityVoteAlgorithm(benchmark::State &state) {
+    for (auto _: state) {
+        Solution().majorityVoteAlgorithm(nums);
+    }
+}
+
+BENCHMARK(majorityVoteAlgorithm);   // NOLINT
+
+BENCHMARK_MAIN();
