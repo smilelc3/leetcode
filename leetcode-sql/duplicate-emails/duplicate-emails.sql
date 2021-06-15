e6975b9dcaabb38e378ebddaf025c6c5bad8a355
smilelc3 <smile@liuchang.men>
2021-06-16 01:26:11 +0800

    182. Duplicate Emails

---
+++ leetcode-sql/duplicate-emails/duplicate-emails.sql
@@ -0,0 +1,21 @@
+# 初始化
+create database if not exists leetcode;
+use leetcode;
+Create table If Not Exists Person
+(
+    Id    int,
+    Email varchar(255)
+);
+Truncate table Person;
+insert into Person (Id, Email)
+values ('1', 'a@b.com');
+insert into Person (Id, Email)
+values ('2', 'c@d.com');
+insert into Person (Id, Email)
+values ('3', 'a@b.com');
+
+# 查找重复元素
+select Email
+from Person
+group by Email
+having count(Email) > 1;
