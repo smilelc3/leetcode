90a440549ac8e9b6f53aa14153003a363f8a30d5
smilelc3 <smile@liuchang.men>
2021-06-15 15:22:07 +0800

    184. Department Highest Salary

---
+++ leetcode-sql/department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,51 @@
+# 初始化
+create database if not exists leetcode;
+use leetcode;
+create table if not exists Employee
+(
+    Id           int,
+    Name         varchar(255),
+    Salary       int,
+    DepartmentId int
+);
+
+create table if not exists Department
+(
+    Id   int,
+    Name varchar(255)
+);
+
+truncate table Employee;
+
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('1', 'Joe', '70000', '1');
+
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('2', 'Jim', '90000', '1');
+
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('3', 'Henry', '80000', '2');
+
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('4', 'Sam', '60000', '2');
+
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('5', 'Max', '90000', '1');
+
+Truncate table Department;
+
+insert into Department (Id, Name)
+values ('1', 'IT');
+
+insert into Department (Id, Name)
+values ('2', 'Sales');
+
+# 查询部门最高工资
+select Department.Name as Department, Employee.Name as Employee, Salary
+from Department
+         left join Employee on Department.Id = Employee.DepartmentId
+where (Department.Id, Salary) in (
+    select DepartmentId, max(Salary)
+    from Employee
+    group by DepartmentId
+);
