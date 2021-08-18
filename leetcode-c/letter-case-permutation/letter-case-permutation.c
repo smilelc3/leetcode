65f36e24d2c9b361437bfc6cbb3b6a4dfb3ca644
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-08-18 14:49:35 +0800

    Update letter-case-permutation.c

--- leetcode-c/letter-case-permutation/letter-case-permutation.c
+++ leetcode-c/letter-case-permutation/letter-case-permutation.c
@@ -2,10 +2,6 @@
 // Created by l30014168 on 2021/7/15.
 //
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
@@ -14,14 +10,10 @@ char **dfsSolution(char *s, int *returnSize, int curIdx, char **res) {
     if (curIdx == strlen(s)) {      // 已到达末尾
         char *sCopy = (char *) malloc(sizeof(char) * (curIdx + 1)); // add '\0'
         if (sCopy == NULL) {    // 未成功申请空间
-            perror("malloc fail");
+            perror("malloc fail\n");
             return NULL;
         }
-        errno_t ret = strcpy_s(sCopy, curIdx + 1, s);
-        if (ret != 0) { // 复制失败
-            perror("strcpy_s fail");
-            return NULL;
-        }
+        strcpy(sCopy, s);
         if (*returnSize == 0) { // 首次申请空间
             res = (char **) malloc(sizeof(char *));
         } else if (((*returnSize) & ((*returnSize) - 1)) == 0) {    // *returnSize 为2的整数幂
