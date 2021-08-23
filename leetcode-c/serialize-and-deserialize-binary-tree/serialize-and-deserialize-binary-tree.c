70366af6f4869e6329d6bdaf1a9f001e40343f6b
lcsmile <35222695+smilelc3@users.noreply.github.com>
2021-08-23 12:41:35 +0800

    Update serialize-and-deserialize-binary-tree.c

--- leetcode-c/serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.c
+++ leetcode-c/serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.c
@@ -30,35 +30,31 @@ int str2Int(const char *str, size_t length) {
 
 /** Encodes a tree to a single string. */
 char *serialize(struct TreeNode *root) {
-    Vector *str = VectorCreate(sizeof(char));
-    char tmp = '[';
-    VectorAppend(str, &tmp);
-
+    Vector *nums = VectorCreate(sizeof(int));
+    int nullNum = INT_MIN;
     if (root != NULL) {
         Vector *preLevelTreeNodes = VectorCreate(sizeof(struct TreeNode *));
         VectorAppend(preLevelTreeNodes, &root);
-        char strBuf[11];
-        sprintf(strBuf, "%d", root->val);
-        for (int idxBuf = 0; idxBuf < strlen(strBuf); ++idxBuf) {
-            VectorAppend(str, &strBuf[idxBuf]);
-        }
+        VectorAppend(nums, &root->val);
         while (!VectorIsEmpty(preLevelTreeNodes)) {
             size_t numPreLevelNodes = preLevelTreeNodes->size;
+            struct TreeNode **pPreLevelNode;
             for (size_t idx = 0; idx < numPreLevelNodes; idx++) {
-                struct TreeNode **pPreLevelNode = VectorAtNoCheck(preLevelTreeNodes, idx);
+                // 左子树
+                pPreLevelNode = VectorAtNoCheck(preLevelTreeNodes, idx);
                 if ((*pPreLevelNode)->left != NULL) {
+                    VectorAppend(nums, &(*pPreLevelNode)->left->val);
                     VectorAppend(preLevelTreeNodes, &(*pPreLevelNode)->left);
-                    sprintf(strBuf, "%d", (*pPreLevelNode)->left->val);
-                    for (int idxBuf = 0; idxBuf < strlen(strBuf); ++idxBuf) {
-                        VectorAppend(str, &strBuf[idxBuf]);
-                    }
+                } else {
+                    VectorAppend(nums, &nullNum);
                 }
+                // 右子树
+                pPreLevelNode = VectorAtNoCheck(preLevelTreeNodes, idx);
                 if ((*pPreLevelNode)->right != NULL) {
+                    VectorAppend(nums, &(*pPreLevelNode)->right->val);
                     VectorAppend(preLevelTreeNodes, &(*pPreLevelNode)->right);
-                    sprintf(strBuf, "%d", (*pPreLevelNode)->right->val);
-                    for (int idxBuf = 0; idxBuf < strlen(strBuf); ++idxBuf) {
-                        VectorAppend(str, &strBuf[idxBuf]);
-                    }
+                } else {
+                    VectorAppend(nums, &nullNum);
                 }
             }
             VectorErase(preLevelTreeNodes, 0, numPreLevelNodes);
@@ -66,11 +62,41 @@ char *serialize(struct TreeNode *root) {
         VectorDestroy(preLevelTreeNodes);
     }
 
+    // 去除末尾nullNum
+    while (nums->size > 0 && *(int *) VectorAt(nums, nums->size - 1) == nullNum) {
+        nums->size--;
+    }
+
+    Vector *str = VectorCreate(sizeof(char));
+    char tmp = '[';
+    VectorAppend(str, &tmp);
+
+    char buf[11];
+    for (size_t idx = 0; idx < nums->size; ++idx) {
+        int num = *(int *) VectorAtNoCheck(nums, idx);
+        if (num != nullNum) {
+            sprintf(buf, "%d", num);
+        } else {
+            strcpy(buf, "null");
+        }
+        for (size_t strIdx = 0; strIdx < strlen(buf); strIdx++) {
+            VectorAppend(str, &buf[strIdx]);
+        }
+        if (idx != nums->size - 1) {
+            tmp = ',';
+            VectorAppend(str, &tmp);
+        }
+    }
+
     tmp = ']';
     VectorAppend(str, &tmp);
-    char *ret = malloc(str->size * str->itemSize);
+    VectorDestroy(nums);
+
+    char *ret = malloc(str->size * str->itemSize + 1);
     memcpy(ret, str->items, str->size * str->itemSize);
+    ret[str->size * str->itemSize] = '\0';
     VectorDestroy(str);
+
     return ret;
 }
 
@@ -88,27 +114,24 @@ struct TreeNode *deserialize(char *data) {
     }
 
     size_t left = 1;
-    int nullNum = -1;
+    int nullNum = INT_MIN;
 
+    int num;
     for (size_t idx = 1; idx <= length - 2; ++idx) {
         if (data[idx] == ',') {
-            int num;
             if (idx - left == 4 &&
                 data[left] == 'n' && data[left + 1] == 'u' && data[left + 2] == 'l' && data[left + 3] == 'l') {
-                num = -1;
+                num = nullNum;
             } else {
                 num = str2Int(data + left, idx - left);
             }
-            if (VectorAppend(nums, &num) != 0) {
-                VectorDestroy(nums);
-                return NULL;
-            }
+            VectorAppend(nums, &num);
             left = idx + 1;
         }
     }
-    for (int idx = 0; idx < nums->size; ++idx) {
-        printf("%d", *(int *) VectorAt(nums, idx));
-    }
+    // 最后一个单独处理
+    num = str2Int(data + left, length - left - 1);
+    VectorAppend(nums, &num);
 
     // step 2: TreeCreateByNums
     struct TreeNode *ret = TreeCreateByNums(nums->items, nums->size, nullNum);
