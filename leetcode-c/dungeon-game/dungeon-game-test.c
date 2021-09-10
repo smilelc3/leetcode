7ed37599087fef4efa370c8d18b2cad1a326837e
lcsmile <smile@liuchang.men>
2021-09-10 23:16:36 +0800

    174. Dungeon Game

--- leetcode-c/dungeon-game/dungeon-game-test.c
+++ leetcode-c/dungeon-game/dungeon-game-test.c
@@ -14,7 +14,7 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void calculateMinimumHPExample1(void) {
+void calculateMinimumHPTestExample1(void) {
     int nums[][3] = {{-2, -3,  3},
                      {-5, -10, 1},
                      {10, 30,  -5}};
@@ -24,15 +24,13 @@ void calculateMinimumHPExample1(void) {
     for (size_t idx = 0; idx < dungeonSize; ++idx) {
         dungeon[idx] = nums[idx];
     }
-
     int ans = calculateMinimumHP(dungeon, dungeonSize, &dungeonColSize);
     int correctAns = 7;
     free(dungeon);
     TEST_ASSERT_EQUAL_INT(correctAns, ans);
-
 }
 
-void calculateMinimumHPExample2(void) {
+void calculateMinimumHPTestExample2(void) {
     int nums[][1] = {{0}};
     int dungeonColSize = 1;
     int dungeonSize = 1;
@@ -40,17 +38,30 @@ void calculateMinimumHPExample2(void) {
     for (size_t idx = 0; idx < dungeonSize; ++idx) {
         dungeon[idx] = nums[idx];
     }
-
     int ans = calculateMinimumHP(dungeon, dungeonSize, &dungeonColSize);
     int correctAns = 1;
     free(dungeon);
     TEST_ASSERT_EQUAL_INT(correctAns, ans);
+}
 
+void calculateMinimumHPTestTest0(void) {
+    int nums[][2] = {{0, 0}};
+    int dungeonColSize = 2;
+    int dungeonSize = 1;
+    int **dungeon = malloc(sizeof(int *) * dungeonSize);
+    for (size_t idx = 0; idx < dungeonSize; ++idx) {
+        dungeon[idx] = nums[idx];
+    }
+    int ans = calculateMinimumHP(dungeon, dungeonSize, &dungeonColSize);
+    int correctAns = 1;
+    free(dungeon);
+    TEST_ASSERT_EQUAL_INT(correctAns, ans);
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(calculateMinimumHPExample1);
-    RUN_TEST(calculateMinimumHPExample2);
+    RUN_TEST(calculateMinimumHPTestExample1);
+    RUN_TEST(calculateMinimumHPTestExample2);
+    RUN_TEST(calculateMinimumHPTestTest0);
     return UNITY_END();
 }
--- leetcode-c/dungeon-game/dungeon-game.c
+++ leetcode-c/dungeon-game/dungeon-game.c
@@ -10,12 +10,20 @@ int calculateMinimumHPDPMin(int small, int big, int **dungeon, bool isSwap) {
     // small 代表 宽度; big 代表 高度
     // 高度 >= 宽度
     int *dp = malloc(sizeof(int) * small);
-    memset(dp, INT32_MAX - 1, small);
+    dp[small - 1] = max(1 - (isSwap ? dungeon[small - 1][big - 1] : dungeon[big - 1][small - 1]),
+                        1);     // 终点所需要的最小生命值
 
-    dp[small - 1] = 1;
-    for (int i = big - 1; i >= 0; --i) {
-        for (int j = small - 1; j >= 0; --j) {
-            dp[j] = max(min(dp[j], dp[j + 1]) - (isSwap ? dungeon[j][i] : dungeon[i][j]), 1);
+
+    // 初始化最下行
+    for (int j = small - 2; j >= 0; --j) {
+        dp[j] = max(dp[j + 1] - (isSwap ? dungeon[j][big - 1] : dungeon[big - 1][j]), 1);
+    }
+    // 从右下角向左上角DP
+    for (int i = big - 2; i >= 0; --i) {
+        dp[small - 1] = max(dp[small - 1] - (isSwap ? dungeon[small - 1][i] : dungeon[i][small - 1]), 1);
+        for (int j = small - 2; j >= 0; --j) {
+            dp[j] = max(min(dp[j], dp[j + 1]) - (isSwap ? dungeon[j][i] : dungeon[i][j]),
+                        1);
         }
     }
 
@@ -29,9 +37,9 @@ int calculateMinimumHP(int **dungeon, int dungeonSize, const int *dungeonColSize
     if (dungeonSize == 0 || *dungeonColSize == 0) {
         return 0;
     }
-    if (*dungeonColSize >= dungeonSize) {
-        return calculateMinimumHPDPMin(dungeonSize, *dungeonColSize, dungeon, false);
+    if (*dungeonColSize <= dungeonSize) {
+        return calculateMinimumHPDPMin(*dungeonColSize, dungeonSize, dungeon, false);
     } else {
-        return calculateMinimumHPDPMin(*dungeonColSize, dungeonSize, dungeon, true);
+        return calculateMinimumHPDPMin(dungeonSize, *dungeonColSize, dungeon, true);
     }
 }
