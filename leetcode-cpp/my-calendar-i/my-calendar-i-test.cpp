2ca884618844ca66f7d8fce4b34859ce58d4bb70
l30014168 <liuchang168@huawei.com>
2021-09-11 14:02:48 +0800

    729. My Calendar I

---
+++ leetcode-cpp/my-calendar-i/my-calendar-i-test.cpp
@@ -0,0 +1,22 @@
+//
+// Created by l30014168 on 2021/9/11.
+//
+
+#include <gtest/gtest.h>
+#include "my-calendar-i.cpp"
+
+namespace {
+    TEST(MyCalendarTest, Example1) {    // NOLINT
+        auto myCalendar = new MyCalendar();
+        ASSERT_TRUE(myCalendar->book(10, 20));
+        ASSERT_FALSE(myCalendar->book(15, 25));
+        ASSERT_TRUE(myCalendar->book(20, 30));
+        delete myCalendar;
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-cpp/my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,26 @@
+//
+// Created by l30014168 on 2021/9/11.
+//
+
+#include <map>
+
+class MyCalendar {
+public:
+    std::map<int, int> end2StartMap;
+
+    MyCalendar() = default;
+
+    // 将end作为key，start作为value
+    // 在map中找到第一个key(end)大于start的item,只要存在且item->second < end 就不能添加新的日程安排。
+    bool book(int start, int end) {
+        auto item = end2StartMap.lower_bound(start);
+        if (item->first == start) {
+            item++;
+        }
+        if (item != end2StartMap.end() and item->second < end) {
+            return false;
+        }
+        end2StartMap[end] = start;
+        return true;
+    }
+};
