27cdf143969386fccbf371ed3c3c716176d019b6
lcsmile <smile@liuchang.men>
2021-09-19 22:56:06 +0800

    面试题 17.17. 多次搜索

--- leetcode-c/built-in/util.h
+++ leetcode-c/built-in/util.h
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
@@ -34,5 +35,7 @@ static inline int strDescCmpFunc(const void *a, const void *b) {
 
 // 快速幂
 int64_t fastPow(int64_t a, size_t b);
+
+// TODO gcd/lcm函数，需要从C++17标准中移植
 
 #endif //LEETCODE_C_UTIL_H
--- leetcode-c/multi-search-lcci/multi-search-lcci.c
+++ leetcode-c/multi-search-lcci/multi-search-lcci.c
@@ -2,12 +2,97 @@
 // Created by l30014168 on 2021/9/18.
 //
 
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
+#include <stdlib.h>
+
+# define MAX_CHAR_SIZE 26
+// 前缀树（字典树）节点， 仅支持26个小写字符
+typedef struct tagTriedNode {
+    int wordIdx; // 原单词有序表idx, 为-1时表示非完整单词
+    struct tagTriedNode *next[MAX_CHAR_SIZE];       // 多分支存储下一个字符
+} TrieNode;
+
+// 创建前缀树
+static inline TrieNode *TrieCreate(void) {
+    TrieNode *ret = calloc(1, sizeof(TrieNode));
+    ret->wordIdx = -1;
+    return ret;
+}
+
+// 前缀树添加单词
+void TrieAddWord(TrieNode *this, const char *word, int wordIdx);
+
+// 前缀树删除
+void TrieDestroy(TrieNode *this);
+
+void TrieAddWord(TrieNode *this, const char *word, int wordIdx) {
+    if (word[0] == '\0') {
+        this->wordIdx = wordIdx;
+        return;
+    }
+    size_t nextIdx = word[0] - 'a';
+    if (this->next[nextIdx] == NULL) {
+        this->next[nextIdx] = calloc(1, sizeof(TrieNode));
+        this->next[nextIdx]->wordIdx = -1;
+    }
+    TrieAddWord(this->next[nextIdx], word + 1, wordIdx);
+}
+
+void TrieDestroy(TrieNode *this) {
+    for (size_t idx = 0; idx < MAX_CHAR_SIZE; ++idx) {
+        if (this->next[idx] != NULL) {
+            TrieDestroy(this->next[idx]);
+        }
+    }
+    free(this);
+}
+
+// 在trie中搜索key开头存在的所有解
+void TrieSearch(char *key, TrieNode *root, int **ret, int *returnColumnSizes, int startIdx) {
+    if (root == NULL) {
+        return;
+    }
+    if (root->wordIdx != -1) {      // 找到解
+        ret[root->wordIdx][returnColumnSizes[root->wordIdx]] = startIdx;
+        returnColumnSizes[root->wordIdx]++;
+    }
+    if (key[0] == '\0') {
+        return;
+    }
+    TrieSearch(key + 1, root->next[key[0] - 'a'], ret, returnColumnSizes, startIdx);
+}
+
+#define MAX_BIG_LEN 1000
+
 int **multiSearch(char *big, char **smalls, int smallsSize, int *returnSize, int **returnColumnSizes) {
     // 根据smalls实现前缀树
+    TrieNode *smallsTrie = TrieCreate();
+    for (size_t idx = 0; idx < smallsSize; ++idx) {
+        if (smalls[idx][0] == '\0') {
+            continue;
+        }
+        TrieAddWord(smallsTrie, smalls[idx], idx);
+    }
 
+    *returnSize = smallsSize;
+    int **ret = malloc(sizeof(int *) * *returnSize);
+    for (int idx = 0; idx < *returnSize; ++idx) {
+        ret[idx] = malloc(MAX_BIG_LEN * sizeof(int));
+    }
+
+    *returnColumnSizes = calloc(*returnSize, sizeof(int));
+
+    for (size_t idx = 0; idx < strlen(big); ++idx) {
+        TrieSearch(big + idx, smallsTrie, ret, *returnColumnSizes, idx);
+        for (int i = 0; i < *returnSize; ++i) {
+            printf("idx(%d): ", i);
+            for (int j = 0; j < (*returnColumnSizes)[i]; ++j) {
+                printf("%d ", ret[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    TrieDestroy(smallsTrie);
+    return ret;
 }
