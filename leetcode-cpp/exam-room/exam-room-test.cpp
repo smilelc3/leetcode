6b7ba46defa32fea5c158ee456db731a44ffba89
l30014168 <liuchang168@huawei.com>
2021-09-29 17:59:03 +0800

    855. Exam Room 继续优化

--- leetcode-cpp/exam-room/exam-room-test.cpp
+++ leetcode-cpp/exam-room/exam-room-test.cpp
@@ -16,6 +16,18 @@ namespace {
         ASSERT_EQ(5, obj->seat());
         delete obj;
     }
+
+    TEST(ExamRoomTest, Test0) {        // NOLINT
+        auto obj = new ExamRoom(4);
+        ASSERT_EQ(0, obj->seat());
+        ASSERT_EQ(3, obj->seat());
+        ASSERT_EQ(1, obj->seat());
+        ASSERT_EQ(2, obj->seat());
+        obj->leave(1);
+        obj->leave(3);
+        ASSERT_EQ(1, obj->seat());
+        delete obj;
+    }
 }
 
 int main(int argc, char *argv[]) {
--- leetcode-cpp/exam-room/exam-room.cpp
+++ leetcode-cpp/exam-room/exam-room.cpp
@@ -6,78 +6,91 @@
 #include <unordered_map>
 #include <vector>
 
-// 用一个二叉平衡树(set)维护所有当前空闲的区间，同时用两个哈希表记录每个位置左边和右边是否有区间（这样在有人离开座位，需要合并区间的时候就会更加方便。）
-struct Segment {
-    int l, r, no, dist;
+// 用一个二叉平衡树(set)维护所有当前空闲的区间，同时用两个哈希表记录每个位置左边和右边有那些区间（这样在有人离开座位，需要合并区间的时候就会更加方便。）
 
-    bool operator<(const Segment &that) const {
-        return this->dist > that.dist or (this->dist == that.dist and this->no < that.no);
+// 区间类
+class Interval {
+public:
+    int left, right;
+    int num;       // num代表下一次分配座位号
+
+    Interval(int left, int right, int num) : left(left), right(right), num(num) {}
+
+    bool operator<(const Interval &that) const {
+        auto diff = std::abs(this->getDis() - that.getDis());
+        if (diff <= 1) {
+            return this->num < that.num;            // 区间长度差距在1以内，返回靠前的
+        }
+        return this->getDis() > that.getDis();        // 排序优先返回最大区间
+    }
+
+private:
+    inline int getDis() const {
+        return right - left;
     }
 };
 
 class ExamRoom {
 private:
-    int N;
-    std::unordered_map<int, std::vector<Segment>> left, right;
-    std::set<Segment> s;
+    int n;      // 座位数量
+    std::unordered_map<int, std::vector<Interval>> leftMap, rightMap;
+    std::set<Interval> intervalSet;
 
-    void add_segment(int l, int r) {
-        l = std::max(l, 1);
-        r = std::min(r, N);
-        if (r < l) {
+    // 添加一个区间
+    void addInterval(int left, int right) {
+        if (left >= right) {
             return;
         }
-        int mid = (l + r) / 2;
-        int dist = mid - l + 1;
-        if (l == 1) {
-            mid = 1;
-            dist = r;
-        } else if (r == N) {
-            mid = r;
-            dist = r - l + 1;
+        auto num = (left + right) / 2;
+        if (left == 0) {
+            num = 0;
+        } else if (right == n - 1) {
+            num = n - 1;
         }
-        Segment new_segment{l, r, mid, dist};
-        s.insert(new_segment);
-        left[r + 1].emplace_back(new_segment);
-        right[l - 1].emplace_back(new_segment);
+        auto interval = Interval(left, right, num);
+        intervalSet.insert(interval);
+        leftMap[right].emplace_back(interval);
+        rightMap[left].emplace_back(interval);
     }
 
-    void remove_segment(Segment &segment) {
-        left[segment.r + 1].clear();
-        right[segment.l - 1].clear();
-        s.erase(segment);
+    // 在set中删除该节点， 在左右节点map删除该区间
+    void removeInterval(Interval &interval) {
+        intervalSet.erase(interval);
+        leftMap[interval.right].clear();        // 这里不采用erase，仅清空value，不删除key,以便后需添加
+        rightMap[interval.left].clear();
     }
 
 public:
-    explicit ExamRoom(int N) {
-        this->N = N;
-        Segment start{1, N, 1, N};
-        s.insert(start);
-        right[0].emplace_back(start);
-        left[N + 1].emplace_back(start);
+    explicit ExamRoom(int n) {
+        this->n = n;
+        auto whole = Interval(0, n - 1, 0);
+        intervalSet.insert(whole);          // 添加 0 ~ n-1 区间段
+        rightMap[0].emplace_back(whole);       // 0 的右边添加该区间
+        leftMap[n - 1].emplace_back(whole);    // n-1 的左边添加该区间
     }
 
+    // 分配一个座位
     int seat() {
-        Segment best = *s.begin();
-        remove_segment(best);
-        add_segment(best.l, best.no - 1);
-        add_segment(best.no + 1, best.r);
-        return best.no - 1;
+        auto best = *intervalSet.begin();
+        removeInterval(best);
+        addInterval(best.left, best.num);
+        addInterval(best.num, best.right);
+        return best.num;
     }
 
+    // 释放一个座位
     void leave(int p) {
-        p++;
-        int l = p, r = p;
-        if (not left[p].empty()) {
-            Segment ls = left[p][0];
-            l = ls.l;
-            remove_segment(ls);
+        auto left = p, right = p;        // 删除p的左右区间后，合并的新区间
+        if (not leftMap[p].empty()) {
+            auto leftInr = leftMap[p].front();
+            left = leftInr.left;
+            removeInterval(leftInr);
         }
-        if (not right[p].empty()) {
-            Segment rs = right[p][0];
-            r = rs.r;
-            remove_segment(rs);
+        if (not rightMap[p].empty()) {
+            auto rightInr = rightMap[p].front();
+            right = rightInr.right;
+            removeInterval(rightInr);
         }
-        add_segment(l, r);
+        addInterval(left, right);
     }
 };
