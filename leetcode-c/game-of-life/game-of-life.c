8137a0f0e52a25d095281286c790ffc4556e9efc
lcsmile <smile@liuchang.men>
2021-10-01 10:08:44 +0800

    289. Game of Life

--- leetcode-c/game-of-life/game-of-life.c
+++ leetcode-c/game-of-life/game-of-life.c
@@ -7,56 +7,40 @@
 #define DEAD 0
 #define ALIVE 1
 
-uint8_t popByte(uint8_t x) {
-    uint8_t n = 0;
-    while (x != 0) {
-        x = x & (x - 1);
-        n++;
+static inline bool isCellAlive(int **board, int x, int y, int row, int col) {
+    if (x < 0 || y < 0 || x >= row || y >= col) {
+        return false;
     }
-    return n;
-}
-
-inline void byteSetzBit(uint8_t *pByte, uint8_t idx, bool val) {
-    if (val == true) {
-        *pByte |= 1 << idx;
-    } else {
-        *pByte &= ~(1 << idx);
-    }
-}
-
-int checkCell(int8_t conv) {
-    // conv第0~7分别表示左上，上，右上，左，右，左下，下，右下细胞状态（0-死亡，1-存活）
-    // +---+---+---+
-    // | 0 | 1 | 2 |
-    // +---+---+---+
-    // | 3 |   | 4 |
-    // +---+---+---+
-    // | 5 | 6 | 7 |
-    // +---+---+---+
-    uint8_t nbrNum = popByte(conv);
-    if (nbrNum < 2) {
-        return DEAD;
-    }
-    if (nbrNum <= 3) {
-        return ALIVE;
-    }
-    return DEAD;
+    return board[x][y] & 1;
 }
 
 void gameOfLife(int **board, int boardSize, const int *boardColSize) {
-    uint8_t conv = 0;
-    byteSetzBit(&conv, 4, (bool)board[0][1]);
-    byteSetzBit(&conv, 6, (bool)board[1][0]);
-    byteSetzBit(&conv, 7, (bool)board[1][1]);
-    //处理最上和最左
-    for (size_t j = 0; j <; ++j) {
+    for (int x = 0; x < boardSize; ++x) {
+        for (int y = 0; y < *boardColSize; ++y) {
+            // 将结果保存在第二位
+            uint8_t nbrAliveCnt = 0;
 
-    }
+            for (int8_t dx = -1; dx <= 1; ++dx) {
+                for (int8_t dy = -1; dy <= 1; ++dy) {
+                    if (dx == 0 && dy == 0) {
+                        continue;
+                    }
+                    if (isCellAlive(board, x + dx, y + dy, boardSize, *boardColSize) == true) {
+                        nbrAliveCnt++;
+                    }
+                }
+            }
+            // 下一次存活的情况
+            if ((board[x][y] == ALIVE && nbrAliveCnt >= 2 && nbrAliveCnt <= 3) ||
+                (board[x][y] == DEAD && nbrAliveCnt == 3)) {
+                board[x][y] += 2;   // 第二位为 1-ALIVE
+            }
 
-    for (size_t i = 1; i < boardSize - 1; ++i) {
-        for (size_t j = 1; j < *boardColSize - 1; ++j) {
         }
     }
-
-    // 处理最右和最下
+    for (size_t i = 0; i < boardSize; ++i) {
+        for (size_t j = 0; j < *boardColSize; ++j) {
+            board[i][j] >>= 1;  // 替换为新结果
+        }
+    }
 }
