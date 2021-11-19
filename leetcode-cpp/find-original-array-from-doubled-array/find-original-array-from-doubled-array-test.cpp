4897faf518c51f141f7fa79cf11a2ef2927720ad
l30014168 <liuchang168@huawei.com>
2021-11-19 14:55:19 +0800

    2007. Find Original Array From Doubled Array

---
+++ leetcode-cpp/find-original-array-from-doubled-array/find-original-array-from-doubled-array-test.cpp
@@ -0,0 +1,34 @@
+//
+// Created by l30014168 on 2021/11/19.
+//
+
+#include <gtest/gtest.h>
+#include "find-original-array-from-doubled-array.cpp"
+
+namespace {
+    TEST(findOriginalArrayTest, Example1) {   // NOLINT
+        std::vector<int> changed = {1, 3, 4, 2, 6, 8};
+        auto ans = Solution().findOriginalArray(changed);
+        decltype(ans) rightAns = {1, 3, 4};
+        ASSERT_EQ(ans, rightAns);
+    }
+
+    TEST(findOriginalArrayTest, Example2) {   // NOLINT
+        std::vector<int> changed = {6, 3, 0, 1};
+        auto ans = Solution().findOriginalArray(changed);
+        decltype(ans) rightAns = {};
+        ASSERT_EQ(ans, rightAns);
+    }
+
+    TEST(findOriginalArrayTest, Example3) {   // NOLINT
+        std::vector<int> changed = {1};
+        auto ans = Solution().findOriginalArray(changed);
+        decltype(ans) rightAns = {};
+        ASSERT_EQ(ans, rightAns);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
