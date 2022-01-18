639d33fb987b96beb1539966876f73cbfc37a061
l30014168 <liuchang168@huawei.com>
2022-01-18 11:14:26 +0800

    682. Baseball Game

---
+++ leetcode-cpp/baseball-game/baseball-game-test.cpp
@@ -0,0 +1,35 @@
+//
+// Created by l30014168 on 2022/1/17.
+//
+
+#include <gtest/gtest.h>
+#include "baseball-game.cpp"
+
+namespace {
+    TEST(calPointsTest, Example1) {     // NOLINT
+        std::vector<std::string> ops = {"5", "2", "C", "D", "+"};
+        auto ans = Solution::calPoints(ops);
+        decltype(ans) correctAns = 30;
+        ASSERT_EQ(ans, correctAns);
+    }
+
+    TEST(calPointsTest, Example2) {     // NOLINT
+        std::vector<std::string> ops = {"5", "-2", "4", "C", "D", "9", "+", "+"};
+        auto ans = Solution::calPoints(ops);
+        decltype(ans) correctAns = 27;
+        ASSERT_EQ(ans, correctAns);
+    }
+
+    TEST(calPointsTest, Example3) {     // NOLINT
+        std::vector<std::string> ops = {"1"};
+        auto ans = Solution::calPoints(ops);
+        decltype(ans) correctAns = 1;
+        ASSERT_EQ(ans, correctAns);
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-cpp/baseball-game/baseball-game.cpp
@@ -0,0 +1,26 @@
+//
+// Created by l30014168 on 2022/1/17.
+//
+
+#include <string>
+#include <vector>
+#include <numeric>
+
+class Solution {
+public:
+    static int calPoints(std::vector<std::string> &ops) {
+        std::vector<int> dataStack;
+        for (auto const &op: ops) {
+            if (op == "D") {
+                dataStack.emplace_back(dataStack.back() * 2);
+            } else if (op == "+") {
+                dataStack.emplace_back(*dataStack.rbegin() + *(dataStack.rbegin() + 1));
+            } else if (op == "C") {
+                dataStack.pop_back();
+            } else {
+                dataStack.emplace_back(std::stoi(op));
+            }
+        }
+        return std::accumulate(dataStack.begin(), dataStack.end(), 0);
+    }
+};
